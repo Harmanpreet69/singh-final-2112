4aa9925784b867720f5857b964a2ed9a5e753a17
Harmanpreet69 <200567042@student.georgianc.on.ca>
2024-12-10 13:09:02 -0500

    Added root page text

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="container">
+      <h1>My Submission for COMP2112 Final Exam</h1>
+      <p>Harmanpreet Singh - 200567042</p>
+    </main>
+  );
 }
